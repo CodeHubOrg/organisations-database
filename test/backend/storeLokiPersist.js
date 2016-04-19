4440449e0d350ceb29cce660bd7ca9f97ae6fb06
Michael Gray <mg13@sanger.ac.uk>
2016-04-19 10:43:45 +0100

    Test actual persistence.

--- test/backend/storeLokiPersist.js
+++ test/backend/storeLokiPersist.js
@@ -1,14 +1,13 @@
 import {expect} from 'chai';
 import loki from 'lokijs';
+import fs   from 'fs';
 
 import LokiPersist from '../../backend/stores/lokiPersist.js';
 
 let testPath = 'testdb.js';
 
 function setup() {
-    const lokidb = new loki('testdb.js');
-    lokidb.addCollection('tests');
-    lokidb.saveDatabase;
+    fs.truncateSync(testPath, 0);
     console.log('setup done');
 }
 
@@ -28,11 +27,30 @@ describe('loki persist wrapper', () => {
             let lp = new LokiPersist(testPath);
             var result = lp.persist( (db) => {
                 let cln = db.addCollection('tests'); // because it doesn't exist yet
-                cln.add({ added: 'stuff'});
+                cln.insert({ added: 'stuff'});
                 return 'bonanza';
             });
             return result.then( (data) => {
                 expect(data).to.equal('bonanza');
+                let cln = lp.db.getCollection('tests');
+                expect(cln).to.exist;
+                expect(cln.data).to.exist;
+                let added = cln.findOne({added: 'stuff'});
+                expect(added).to.exist;
+                done();
+            }).catch( (err) => console.error(err));
+        });
+    });
+
+    describe('something is still there', () => {
+        it('gets something back from the db', (done) => {
+            let lp = new LokiPersist(testPath);
+            var result = lp.persist( (db) => {
+                let cln = db.getCollection('tests');
+                return cln.data;
+            });
+            return result.then( (data) => {
+                expect(data.length).to.equal(1);
                 done();
             })
         });
