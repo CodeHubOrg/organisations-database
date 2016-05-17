30e2118df8e1b89492dc0406775a1d135890c31f
Michael Gray <mg13@sanger.ac.uk>
2016-05-17 11:55:01 +0100

    import cleanup

--- components/OrganisationList.js
+++ components/OrganisationList.js
@@ -1,19 +1,5 @@
-import React, { Component, PropTypes } from 'react'
-import classnames from 'classnames'
-import { jsdom } from 'jsdom'
+import React, { Component } from 'react'
 import OrganisationListRow from './OrganisationListRow'
-
-//Snippet taken from https://www.npmjs.com/package/jquery
-// require("jsdom").env("", function(err, window) {
-// 	if (err) {
-// 		console.error(err);
-// 		return;
-// 	}
-//
-// 	var $ = require("jquery")(window);
-// 	require('bootstrap');
-// });
-
 
 class OrganisationList extends Component {
 
@@ -33,7 +19,6 @@ class OrganisationList extends Component {
 				</thead>
 				<tbody className="tbody">
 					{rows}
-					//<OrganisationListRow name={this.props.organisations[0].name} id={this.props.organisations[0].id}/>
 				</tbody>
 			</table>
         );
