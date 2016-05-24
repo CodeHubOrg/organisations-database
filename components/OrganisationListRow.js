12991a6411550fb7cd1d99839f0a5ec055b21740
Oliver Denman <oliver@therewillbeanimals.com>
2016-05-24 12:03:32 +0100

    Binding this to callbacks

--- components/OrganisationListRow.js
+++ components/OrganisationListRow.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 class OrganisationListRow extends Component {
 
     render() {
+        console.log('Row: '+this.props.id+' selected? :'+this.props.selected)
         return(
                 <tr onClick={
                   (this.props.selected) ? 
-                  this.props.onDeselectOrganisation : 
-                  this.props.onSelectOrganisation
+                  this.props.onDeselectOrganisation.bind(this) : 
+                  this.props.onSelectOrganisation.bind(this)
                 }
                 >
                     <td className="name">{this.props.name}</td>
