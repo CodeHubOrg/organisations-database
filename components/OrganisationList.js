502ddcc5aa8e1967c9ef6e885ca37696b5d1bcae
Oliver Denman <oliver@therewillbeanimals.com>
2016-05-24 21:56:28 +0100

    Stubbed select callbacks

--- components/OrganisationList.js
+++ components/OrganisationList.js
@@ -4,10 +4,24 @@ import OrganisationListRow from './OrganisationListRow'
 export default class OrganisationList extends Component {
 
     render() {
+    	let onSelectOrganisation = function () {
+    		console.log('selecting')
+    	}
+
+    	let onDeselectOrganisation = function () {
+    		console.log('deselecting')
+    	}
+
     	let orgs = this.props.organisations;
     	let numberOfOrgs = orgs.length;
     	let rows = orgs.map(
-    		(org, index) => <OrganisationListRow name={org.name} id={org.id} key={index} />
+    		(org, index) => <OrganisationListRow 
+						    		name={org.name} 
+						    		id={org.id} 
+						    		key={index}
+						    		onSelectOrganisation={onSelectOrganisation}
+						    		onDeselectOrganisation={onDeselectOrganisation}
+						    		/>
     	);		
 
 		return(
