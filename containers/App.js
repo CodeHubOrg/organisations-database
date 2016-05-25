e42ac061e3aa4cc44b0c1abbcd8e941e23d38ab9
Oliver Denman <oliver@therewillbeanimals.com>
2016-05-25 10:51:01 +0100

    Implemented selection callbacks

--- containers/App.js
+++ containers/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 import OrganisationList from '../components/OrganisationList'
+import * as actions from '../actions'
 
 export default class App extends Component {
 
     render() {
-        const {organisations, dispatch } = this.props
+        const {organisations, dispatch} = this.props
+
+        function onSelectOrganisation() {
+            console.log('selecting')
+            let id = this.props.id
+            let action = actions.selectOrganisation(this.props.id)
+            dispatch(action)
+        }
+
+        let onDeselectOrganisation = function () {
+            console.log('deselecting')
+            let id = this.props.id
+            let action = actions.deSelectOrganisation(this.props.id)
+            dispatch(action)
+        }
 
         return (
             <div>
                 <h1>Organisations Database</h1>
                 <OrganisationList
+                    dispatch={dispatch}
                     organisations={organisations}
+                    onSelectOrganisation={onSelectOrganisation}
+                    onDeselectOrganisation={onDeselectOrganisation}
                 />
             </div>
         )
