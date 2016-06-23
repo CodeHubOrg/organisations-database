7ae633f8e41ef09c8f69b1767c9c6159f1183ed2
Ben Townsend <btownsend220@gmail.com>
2016-06-23 17:09:05 +0100

    initial component created for admin area

---
+++ containers/Admin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Admin extends Component {
+
+    render() {
+        return (
+            <h1>This is the admin area.</h1> 
+        ) 
+    }
+
+}
