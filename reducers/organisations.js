0b843ecf9ec35db987a7a6043abcbcea76ead41a
Katja Durrani <mail@kdurrani.co.uk>
2016-07-18 00:46:46 +0100

    reinstate initialState in organisations file for testing

--- reducers/organisations.js
+++ reducers/organisations.js
@@ -6,8 +6,17 @@ import {
     EDIT_ORGANISATION
 } from '../constants/ActionTypes.js';
 
+const initialState = [
+	{
+		name: 'JavaScript 101',
+		selected: false,
+		id: 1,
+		description: "Group for learning JavaScript",
+		url: "http://www.meetup.com/CodeHub-Bristol/",
+		imagepath: "../constants/javascript101_logo.jpeg",
+	}	
+];
 
-const initialState = [];
 
 export default function organisations ( state = initialState, action ) {
     switch (action.type) {
